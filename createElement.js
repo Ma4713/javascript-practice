7a3c3bfa6d5528c0cd54c0ce5f1b5dfe8a493474
Ma4713 <ma4710320@gmail.com>
2023-10-23 20:38:19 +0100

    Created a new file and have made changes to two new files

--- createElement.js
+++ createElement.js
@@ -1,5 +1,33 @@
 
 
+
+
+    let firstH1 = document.createElement("h1")
+    firstH1.innerHTML = "This  first h1 in this document"
+    document.body.appendChild(firstH1)
+    
+    // let pWIthId = document.getElementById("message")
+    let elementwithClasses = document.getElementByClassName("nicenames")
+    elementwithClasses[0].innerHTML = "I am not a message"
+    elementwithClasses[0].style.color = "green"
+    elementwithClasses[1].innerHTML = "Musa Ali"
+    elementwithClasses[1].style.color = "Blue"
+    document.body.style.backgroundColor = "yellow"
+
+    // let allPs = document.getElementsByTagName("p")
+    // allPs[0].style.color = "purple"
+    // allPs[1].style.color = "purple"
+    // allPs[2].style.color = "purple"
+
+    let allPs = document.querySelector("p")
+    allPs[0].style.color = "red"
+    allPs[1].style.color = "red"
+    allPs[2].style.color = "red"
+
+
+
+    //document.body
+
 // for (let index = 0; index < 200; index++) {
     // let firstDiv = document.createElement("div")
     // firstDiv.innerText = "This DIV is the first Div in this document"
@@ -7,11 +35,10 @@
 
 // }
 
-
-
-// let niceSpan = document.createElement("span")
-// niceSpan.innerHTML = "<p> hello world, I am the first line</p>"
-// document.body.insertBefore(niceSpan,firstDiv)
+// for (let index = 0; index < 200; index++) 
+// // let niceSpan = document.createElement("span")
+// // niceSpan.innerHTML = "<p> hello world, I am the first line</p>"
+// // document.body.insertBefore(niceSpan,firstDiv)
 
 //document.body.removeChild(niceSpan)
 
@@ -23,9 +50,9 @@
 // allTheReds[1].innerHTML = "Yes or no"
 // console.log(allTheReds[1].innerHTML)
 
-let iLikes = document.getElementsByName("ilike")
-console.log(iLikes[0].innerHTML)
-console.log(iLikes[1].innerHTML)
+// let iLikes = document.getElementsByName("ilike")
+// console.log(iLikes[0].innerHTML)
+// console.log(iLikes[1].innerHTML)
 
 // let count = 0;
 // while (count < 200) {
